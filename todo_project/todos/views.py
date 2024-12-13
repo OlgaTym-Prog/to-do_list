e7ab6a7a22c1906f5ea0d8224292bab27d3b144c
Olga <olenka_kuzneczova_89@mail.ru>
2024-12-13 19:10:50 +0300

    update views.py

--- todo_project/todos/views.py
+++ todo_project/todos/views.py
@@ -1,6 +1,8 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, status
+from rest_framework.decorators import action
 from rest_framework.filters import OrderingFilter
 from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.response import Response
 from .models import Task
 from .serializers import TaskSerializer
 
@@ -12,3 +14,18 @@ class TaskViewSet(viewsets.ModelViewSet):
     filterset_fields = ['completed', 'tags']
     ordering_fields = ['due_date', 'created_at', 'title']
     ordering = ['-created_at']
+
+    @action(detail=True, methods=['patch'], url_path='mark_complete')
+    def mark_complete(self, request, pk=None):
+        """
+        Перевод задачи в выполненные.
+        """
+        try:
+            task = self.get_object()
+            task.completed = True
+            task.save()
+            return Response({'detail': 'Задача успешно выполнена.'}, status=status.HTTP_200_OK)
+        except Task.DoesNotExist:
+            return Response({'detail': 'Задача не найдена.'}, status=status.HTTP_404_NOT_FOUND)
+
+
